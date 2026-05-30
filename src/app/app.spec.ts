bb5a491d3fbfc8783e5d585a19f055d2136fd1a1
Tiebe Tollenaere <tiebe.tollenaere@student.howest.be>
2026-05-30 15:27:08 +0200

    fix typo

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, EPortolio-TiebeTollenaere');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, EPortfolio-TiebeTollenaere');
   });
 });
